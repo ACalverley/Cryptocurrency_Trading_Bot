e9867518ffe79d771835a460522ebe16950f44f2
Aidan <14ac104@queensu.ca>
2018-08-12 15:01:24 -0400

    updated syntax of api calls

--- api.js
+++ api.js
@@ -1,16 +1,23 @@
-const binance = require('node-binance-api');
-const bittrex = require('node-bittrex-api');
+const binance_module = require('node-binance-api');
+// const bittrex = require('node-bittrex-api');
 
-binance.options({
-    'APIKEY': process.env.BINANCE_KEY,
-    'APISECRET': process.env.BINANCE_SECRET
-});
+// console.log(process.env.BINANCE_KEY);
+
+const binance = new binance_module().options({
+					    'APIKEY': process.env.BINANCE_KEY,
+					    'APISECRET': process.env.BINANCE_SECRET,
+					    useServerTime: true
+					});
  
-bittrex.options({
-    'apikey' : '',
-    'apisecret' : ''
-});
+// bittrex.options({
+//     'apikey' : '',
+//     'apisecret' : ''
+// });
 
+
+// instance1.prices('BNBBTC', (error, ticker) => {
+//   console.log("Price of BNB: ", ticker.BNBBTC);
+// });
 
 module.exports.binance = binance;
-module.exports.bittrex = bittrex;
+// module.exports.bittrex = bittrex;
